bb29a19211f7c1909b5d617e2e34bf6d8fd67436
hcouto20 <helder@brassun.com.br>
2021-10-23 13:45:49 -0300

    Map com fez menor e maior qtd de horas

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,21 @@
 defmodule GenReport do
 
-  def build do
-    # TO DO
+  alias GenReport.Parser
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.map(fn line -> sum_values(line) end)
+    |> fetch_higher_cost()
   end
+
+  def fetch_higher_cost(map) do
+    {:ok, Enum.min_max_by(map, fn line -> line.hours end)}
+  end
+
+  def sum_values([name, hours, date]) do
+    users = %{name: name, hours: hours, date: date}
+
+    users
+  end
+
 end
